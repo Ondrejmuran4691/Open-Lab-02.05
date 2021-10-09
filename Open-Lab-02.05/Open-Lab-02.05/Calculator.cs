8dfc023f3216406fd4343008a94b14dfd289c033
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-10-09 10:47:51 +0200

    done

---
+++ Open-Lab-02.05/Open-Lab-02.05/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_02._05
+{
+    public class Calculator
+    {
+        public bool DivisibleByFive(int number)
+        {
+            int zvysok = number % 5;
+
+        }
+    }
+}
